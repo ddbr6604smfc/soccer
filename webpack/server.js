eee1bb17ef40747edd864ebdeeba8756411110e1
ddbr6604smfc <ddbr6604smfc@gmail.com>
2015-10-17 18:13:26 -0500

    update dev server config

--- webpack/server.js
+++ webpack/server.js
@@ -1,12 +1,15 @@
 /* eslint-disable no-var, func-names, no-console */
 
 var path = require('path');
+var ip = require('ip');
 var express = require('express');
 var webpack = require('webpack');
 var config = require('./webpack.config.watch');
 
 var app = express();
 var compiler = webpack(config);
+
+var ipAddress = ip.address();
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
@@ -19,11 +22,11 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(3000, 'localhost', function(err) {
+app.listen(3000, ipAddress, function(err) {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log('Listening at http://localhost:3000');
+  console.log(`Listening at http://${ipAddress}:3000`);
 });
